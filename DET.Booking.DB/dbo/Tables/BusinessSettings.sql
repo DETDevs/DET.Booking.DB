26648fb41a3f21253b30b3393a73a5d5b6d35de5
douglas-i <hdouglasu@gmail.com>
2025-05-20 05:58:48 -0600

    refactor: modify BusinessSettings table

--- DET.Booking.DB/dbo/Tables/BusinessSettings.sql
+++ DET.Booking.DB/dbo/Tables/BusinessSettings.sql
@@ -1,13 +1,13 @@
 ﻿CREATE TABLE [dbo].[BusinessSettings]
 (
 	[BusinessID] INT NOT NULL,
-	[Key] VARCHAR NOT NULL,
-    [Value] VARCHAR NOT NULL,
+	[Key] VARCHAR(255) NOT NULL,
+    [Value] VARCHAR(255) NOT NULL,
     [IsActive] BIT NOT NULL,
-	[CreateUser] VARCHAR NOT NULL,
-    [CreateDate] VARCHAR NOT NULL,
-    [ModificationUser] VARCHAR NOT NULL,
-    [ModificationDate] VARCHAR NOT NULL
+	[CreateUser] VARCHAR(50) NOT NULL,
+    [CreateDate] DATETIME NOT NULL,
+    [ModificationUser] VARCHAR(50) NOT NULL,
+    [ModificationDate] DATETIME NOT NULL
 )
 GO
 ALTER TABLE BusinessSettings
