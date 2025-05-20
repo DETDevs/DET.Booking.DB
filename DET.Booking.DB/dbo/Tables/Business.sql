d178bda3cf60467a1a7d034478577ee59e60a7c1
douglas-i <hdouglasu@gmail.com>
2025-05-20 05:55:30 -0600

    refactor: modify Business table and add stored procedure sp_Business_Guardar

--- DET.Booking.DB/dbo/Tables/Business.sql
+++ DET.Booking.DB/dbo/Tables/Business.sql
@@ -1,14 +1,14 @@
 ﻿CREATE TABLE [dbo].[Business]
 (
-	[BusinessID] INT NOT NULL,
-    [Name]  VARCHAR NOT NULL,
-    [Code] VARCHAR NOT NULL,
-    [PrimaryColor] VARCHAR NOT NULL,
-    [SecondColor] VARCHAR NOT NULL,
-    [Logo] VARCHAR NOT NULL,
-    [IsActive] BIT NOT NULL,
-    [CreateUser] VARCHAR NOT NULL,
-    [CreateDate] VARCHAR NOT NULL,
-    [ModificationUser] VARCHAR NOT NULL,
-    [ModificationDate] VARCHAR NOT NULL
+	[BusinessID] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    [Name] VARCHAR(100) NOT NULL,
+    [Code] VARCHAR(50) NOT NULL,
+    [PrimaryColor] VARCHAR(20) NOT NULL,
+    [SecondColor] VARCHAR(20) NOT NULL,
+    [Logo] VARCHAR(255) NOT NULL,
+    [IsActive] BIT NOT NULL DEFAULT 1,
+    [CreateUser] VARCHAR(50) NOT NULL,
+    [CreateDate] DATETIME NOT NULL DEFAULT GETDATE(),
+    [ModificationUser] VARCHAR(50) NULL,
+    [ModificationDate] DATETIME NULL
 )
