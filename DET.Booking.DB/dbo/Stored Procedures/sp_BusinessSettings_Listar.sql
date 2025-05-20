ce99d49db2bd38de3afd9b667c823fda2e2abe0a
douglas-i <hdouglasu@gmail.com>
2025-05-20 06:06:57 -0600

    feat: add stored procedures to list Business

---
+++ DET.Booking.DB/dbo/Stored Procedures/sp_BusinessSettings_Listar.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[sp_BusinessSettings_Listar]
+    @BusinessID INT = NULL,  -- Filtro opcional por negocio
+    @Key VARCHAR(100) = NULL -- Filtro opcional por clave
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT
+        BusinessID,
+        [Key],
+        [Value],
+        IsActive,
+        CreateUser,
+        CreateDate,
+        ModificationUser,
+        ModificationDate
+    FROM BusinessSettings
+    WHERE (@BusinessID IS NULL OR BusinessID = @BusinessID)
+      AND (@Key IS NULL OR [Key] = @Key)
+    ORDER BY BusinessID, [Key];
+END;
---
+++ DET.Booking.DB/dbo/Stored Procedures/sp_Business_Listar.sql
@@ -0,0 +1,32 @@
+﻿CREATE PROCEDURE [dbo].[sp_Business_Listar]
+    @IsActive BIT = NULL,
+    @Code VARCHAR(50) = NULL -- filtro opcional por código de negocio
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT
+        b.BusinessID,
+        b.Name,
+        b.Code,
+        b.PrimaryColor,
+        b.SecondColor,
+        b.Logo,
+        b.IsActive,
+        b.CreateUser,
+        b.CreateDate,
+        b.ModificationUser,
+        b.ModificationDate,
+        bs.[Key],
+        bs.[Value],
+        bs.IsActive AS SettingIsActive,
+        bs.CreateUser AS SettingCreateUser,
+        bs.CreateDate AS SettingCreateDate,
+        bs.ModificationUser AS SettingModificationUser,
+        bs.ModificationDate AS SettingModificationDate
+    FROM Business b
+    LEFT JOIN BusinessSettings bs ON b.BusinessID = bs.BusinessID
+    WHERE (@IsActive IS NULL OR b.IsActive = @IsActive)
+      AND (@Code IS NULL OR b.Code = @Code)
+    ORDER BY b.Name, bs.[Key];
+END;
