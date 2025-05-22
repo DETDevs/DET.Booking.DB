d98de148ea14f93e104070d6a535c21226a3b179
douglas-i <hdouglasu@gmail.com>
2025-05-21 23:04:47 -0600

    fix: stored procedure to list services

--- DET.Booking.DB/dbo/Stored Procedures/sp_Services_Listar.sql
+++ DET.Booking.DB/dbo/Stored Procedures/sp_Services_Listar.sql
@@ -1,4 +1,4 @@
-﻿CREATE PROCEDURE [dbo].[sp_Services_Listar]
+﻿CREATE OR ALTER PROCEDURE [dbo].[sp_Services_Listar]
     @BusinessID INT = NULL,
     @EmployeeID INT = NULL,
     @IsActive BIT = NULL
@@ -7,26 +7,27 @@ BEGIN
     SET NOCOUNT ON;
 
     SELECT
-        s.ServiceID,
-        s.BusinessID,
-        b.Name AS BusinessName,
-        s.EmployeeID,
-        e.Name AS EmployeeName,
-        s.Name AS ServiceName,
-        s.Description,
-        s.DutarionMin,
-        s.Price,
-        s.DiasHabiles,
-        s.IsActive,
-        s.CreateUser,
-        s.CreateDate,
-        s.ModificationUser,
-        s.ModificationDate
-    FROM Services s
-    INNER JOIN Business b ON s.BusinessID = b.BusinessID
-    INNER JOIN Employee e ON s.EmployeeID = e.EmployeeID
-    WHERE (@BusinessID IS NULL OR s.BusinessID = @BusinessID)
-      AND (@EmployeeID IS NULL OR s.EmployeeID = @EmployeeID)
-      AND (@IsActive IS NULL OR s.IsActive = @IsActive)
-    ORDER BY s.Name;
+     s.ServiceID,
+     s.BusinessID,
+     b.Name AS BusinessName,
+     s.EmployeeID,
+     P.Name AS EmployeeName,
+     s.Name AS ServiceName,
+     s.Description,
+     s.DutarionMin,
+     s.Price,
+     s.DiasHabiles,
+     s.IsActive,
+     s.CreateUser,
+     s.CreateDate,
+     s.ModificationUser,
+     s.ModificationDate
+     FROM Services s
+     INNER JOIN Business b ON s.BusinessID = b.BusinessID
+     INNER JOIN Employee e ON s.EmployeeID = e.EmployeeID
+     INNER JOIN Person p ON p.PersonID = e.PersonaID
+     WHERE (@BusinessID IS NULL OR s.BusinessID = @BusinessID)
+       AND (@EmployeeID IS NULL OR s.EmployeeID = @EmployeeID)
+       AND (@IsActive IS NULL OR s.IsActive = @IsActive)
+     ORDER BY s.Name;
 END;
